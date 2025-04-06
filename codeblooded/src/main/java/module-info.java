b9b6ff3ca465c3a7f40bb5644fb46fd2ee654ccc
meno2003 <82838749+meno2003@users.noreply.github.com>
2025-04-06 17:31:57 +0100

    Delete codeblooded directory

--- codeblooded/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.codeblooded {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.codeblooded to javafx.fxml;
-    exports com.example.codeblooded;
-}
